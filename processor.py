268258a12d430266fb30a9ac2234b324385cd092
Rasmus Ekström <beckettlavoie4736@gmail.com>
2026-08-19 00:16:43 +0200

    input validation in main processing loop

--- processor.py
+++ processor.py
@@ -1,21 +1,29 @@
-import json
-from validators import validate_input
+import time
+import random
 
-class Processor:
-    def __init__(self, data):
-        self.data = data
+class AutoClicker:
+    def __init__(self, click_interval: float, max_clicks: int):
+        self.click_interval = click_interval
+        self.max_clicks = max_clicks
+        self.click_count = 0
 
-    def process(self):
-        for item in self.data:
-            if not validate_input(item):
-                continue
-            self.handle_item(item)
+    def validate_inputs(self):
+        if not (0 < self.click_interval < 5):
+            raise ValueError('Click interval must be between 0 and 5 seconds.')
+        if not (1 <= self.max_clicks <= 1000):
+            raise ValueError('Max clicks must be between 1 and 1000.')
 
-    def handle_item(self, item):
-        # Processing logic here
-        print(f'Processing: {item}')
+    def run(self):
+        self.validate_inputs()
+        while self.click_count < self.max_clicks:
+            time.sleep(self.click_interval)
+            self.perform_click()
 
+    def perform_click(self):
+        print('Click!')
+        self.click_count += 1
+
+# Example usage:
 if __name__ == '__main__':
-    raw_data = ['valid_data1', 'invalid_data', 'valid_data2']
-    processor = Processor(raw_data)
-    processor.process()
+    clicker = AutoClicker(click_interval=1, max_clicks=10)
+    clicker.run()
